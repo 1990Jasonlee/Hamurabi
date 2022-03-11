43621c0c22ab75b4215a4b3946224fe88b933388
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-11 18:08:04 -0500

    summary done

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -11,6 +11,26 @@ import java.util.Scanner;
         }
 
         void playGame() {
+            int year = 1;
+            int starvationDeath = 0;
+            int immigrants = 5;
+            int population = 100;
+            int harvest = 3000;
+            int acres = 1000;
+            int grainEatenByRats = 200;
+            int bushel = 3000;
+            int newCostOfLand = 19;
+
+            System.out.println("O great Hammurabi!\n" +
+                "You are in year "+ year +" of your ten year rule.\n" +
+                "In the previous year " + starvationDeath + " people starved to death.\n" +
+                "In the previous year "+ immigrants +" people entered the kingdom.\n" +
+                "The population is now " + population + ".\n" +
+                "We harvested "+ harvest +" bushels at " + acres/harvest + " bushels per acre.\n" +
+                "Rats destroyed "+ grainEatenByRats + " bushels, leaving "+ (bushel-grainEatenByRats) +" bushels in storage.\n" +
+                "The city owns "+ acres +" acres of land.\n" +
+                "Land is currently worth "+ newCostOfLand +" bushels per acre.");
+
             // declare local variables here: grain, population, etc.
             // statements go after the declations
         }
